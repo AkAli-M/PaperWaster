39e05fa6e08066a4be7adf0cf2b4689d08fc11f9
AkAli-M <alikochottayil@gmail.com>
2025-08-08 13:38:03 +0000

    Initial commit

---
+++ termux_camera.py
@@ -0,0 +1,26 @@
+import subprocess
+import os
+import datetime
+
+# Define the output file path
+current_time = datetime.datetime.now().strftime("%H_%M_%S")
+ImageName = f"photo_{current_time}.jpg"
+
+# The command to be executed
+# -c 0 specifies the back camera. You can change it to 1 for the front camera if available.
+
+def CaptureImage(FileName):
+    command = ["termux-camera-photo", "-c", "0", FileName]
+    """Takes a picture using the termux-camera-photo command."""
+    print("Attempting to take a picture...")
+    try:
+        # Execute the command(Termux specific)
+        result = subprocess.run(command, capture_output=True, text=True, check=True)
+        print(f"Successfully took picture and saved it to {FileName}")
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    
+
+if __name__ == "__main__":
+    CaptureImage(ImageName)
